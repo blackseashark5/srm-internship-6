b254d8ac505a7726b5e738b0458fdafcc2ae5090
blackseashark5 <133557445+blackseashark5@users.noreply.github.com>
2024-03-25 21:59:11 +0530

    Delete Hospital-Management-System-master directory

--- Hospital-Management-System-master/src/Redux/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./auth/reducer";
-import dataReducer from "./Datas/reducer";
-export const rootReducer = combineReducers({
-  auth: authReducer,
-  data: dataReducer,
-});
